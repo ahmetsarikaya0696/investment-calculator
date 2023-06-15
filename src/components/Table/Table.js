0ec702dc883344c02eed2755ff5b7f5683eab545
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-15 21:19:42 +0300

    No investment calculated yet kısmı yazıldı

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,30 +4,34 @@ import styles from "./Table.module.css";
 const Table = (props) => {
   const datas = props.data;
 
-  console.log(props.data);
   return (
-    <table className={styles.result}>
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Total Savings</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
-      </thead>
-      <tbody>
-        {datas.map((data) => (
-          <tr>
-            <td>{data.year}</td>
-            <td>{data.savingsEndOfYear + " $"}</td>
-            <td>{data.yearlyInterest + " $"}</td>
-            <td>{data.totalInterest + " $"}</td>
-            <td>{data.investedCapital + " $"}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      {datas.length === 0 && <div className={styles.card}>No investment calculated yet</div>}
+      {datas.length !== 0 && (
+        <table className={styles.result}>
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Total Savings</th>
+              <th>Interest (Year)</th>
+              <th>Total Interest</th>
+              <th>Invested Capital</th>
+            </tr>
+          </thead>
+          <tbody>
+            {datas.map((data) => (
+              <tr>
+                <td>{data.year}</td>
+                <td>{data.savingsEndOfYear + " $"}</td>
+                <td>{data.yearlyInterest + " $"}</td>
+                <td>{data.totalInterest + " $"}</td>
+                <td>{data.investedCapital + " $"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
